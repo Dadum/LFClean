98f864bd1a29119a8bad2210db28804bd1215b18
dadum <sir.dadum@gmail.com>
2021-05-09 18:44:41 +0200

    [config] Add blacklist management

--- config.lua
+++ config.lua
@@ -3,54 +3,103 @@ local C = LibStub("AceConfig-3.0")
 local CD = LibStub("AceConfigDialog-3.0")
 local DB = LibStub("AceDB-3.0")
 
+local shortcutsOptions = {
+    entryButtons = {
+        name = "Entry Buttons",
+        desc = "Add one button for each entry of the group finder.",
+        type = "toggle",
+        width = 1.5,
+        order = 10,
+        set = function(info, val)
+            LFClean.conf.profile.entryButtons = val
+            LFClean:GenerateEntryButtons()
+        end,
+        get = function(info)
+            return LFClean.conf.profile.entryButtons
+        end
+    },
+    selectedButton = {
+        name = "Selected Button",
+        desc = "Add a button to report the selected group entry, nect to the refresh button.",
+        type = "toggle",
+        width = 1.5,
+        order = 20,
+        set = function(info, val)
+            LFClean.conf.profile.selectedButton = val
+            LFClean:GenerateSelectedButton()
+        end,
+        get = function(info)
+            return LFClean.conf.profile.selectedButton
+        end
+    },
+    reportType = {
+        name = "Report Groups For",
+        desc = "Defines what the groups should be reported for when using the shortcut buttons",
+        type = "select",
+        order = 30,
+        set = function(info, val)
+            LFClean.conf.profile.reportType = val
+        end,
+        get = function(info)
+            return LFClean.conf.profile.reportType
+        end,
+        values = {
+            ["lfglistspam"] = "Advertisement",
+            ["lfglistname"] = "Group Name",
+            ["lfglistcomment"] = "Description",
+            ["badplayername"] = "Leader Name"
+        }
+    }
+}
+
+local blacklistOptions = {
+    manageBlacklist = {
+        name = "Manage Blacklist",
+        type = "select",
+        order = 10,
+        set = function(info, val)
+            LFClean.blmSelect = val
+        end,
+        get = function(info)
+            return LFClean.blmSelect
+        end,
+        values = function()
+            local blacklist = {}
+            for k, _ in pairs(LFClean.conf.profile.blacklist) do
+                blacklist[k] = k
+            end
+            return blacklist
+        end
+    },
+    deleteEntry = {
+        name = "remove",
+        type = "execute",
+        func = function()
+            if LFClean.blmSelect then
+                LFClean.conf.profile.blacklist[LFClean.blmSelect] = nil
+            else
+                LFClean:Print("No blacklist entry selected")
+            end
+        end
+    }
+}
+
 local options = {
     type = "group",
     args = {
-        entryButtons = {
-            name = "Entry Buttons",
-            desc = "Add one button for each entry of the group finder.\nNOTE: Might break the entry layout for long group names",
-            type = "toggle",
-            width = "full",
-            order = 0,
-            set = function(info, val)
-                LFClean.conf.profile.entryButtons = val
-                LFClean:GenerateEntryButtons()
-            end,
-            get = function(info)
-                return LFClean.conf.profile.entryButtons
-            end
+        shortcuts = {
+            type = "group",
+            name = "Shortcuts",
+            order = 10,
+            inline = true,
+            args = shortcutsOptions
         },
-        selectedButton = {
-            name = "Selected Button",
-            desc = "Add a button to report the selected group entry",
-            type = "toggle",
-            width = "full",
-            order = 1,
-            set = function(info, val)
-                LFClean.conf.profile.selectedButton = val
-                LFClean:GenerateSelectedButton()
-            end,
-            get = function(info)
-                return LFClean.conf.profile.selectedButton
-            end
-        },
-        reportType = {
-            name = "Report Groups For",
-            desc = "Defines what the groups should be reported for when using the shortcut buttons",
-            type = "select",
-            order = 2,
-            set = function(info, val)
-                LFClean.conf.profile.reportType = val
-            end,
-            get = function(info)
-                return LFClean.conf.profile.reportType
-            end,
-            values = {
-                ["lfglistspam"] = "Advertisement",
-                ["lfglistname"] = "Group Name",
-                ["lfglistcomment"] = "Description",
-                ["badplayername"] = "Leader Name"
-            }
+        blacklist = {
+            type = "group",
+            name = "Blacklist",
+            order = 20,
+            inline = true,
+            args = blacklistOptions
         }
     }
 }
